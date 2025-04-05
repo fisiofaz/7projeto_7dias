c4edbf8e3a66650e201fb2e228ba7868f593383a
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2025-04-05 13:17:34 -0300

    frisrt: segundo projeto

---
+++ Relógio/script.js
@@ -0,0 +1,31 @@
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m'); 
+let hElement = document.querySelector('.p_h');
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+
+    // Relógio Digital
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`
+
+    // Rotate the hands
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90 + ((360 / 12) * (minute / 60));
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
+}
+
+function fixZero(time ) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // Call it once to avoid delay on first load
